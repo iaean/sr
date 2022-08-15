7f4a2b0ab3d93feb841da0f96233c5383110e55c
iaean <iaean@users.noreply.github.com>
2022-08-15 20:48:28 +0200

    fix(ci): update config

--- pre-release.config.js
+++ pre-release.config.js
@@ -5,5 +5,8 @@ const baseReleaseConfig = require('./release.config.js');
 module.exports = {
   ...baseReleaseConfig,
   skipTag: true,
-  plugins: ['@semantic-release/commit-analyzer', './release-scripts/application-info']
+  plugins: [
+    '@semantic-release/commit-analyzer', '@semantic-release/release-notes-generator',
+    [ '@semantic-release/exec', { prepareCmd: "./bump.sh ${nextRelease.version}" } ]
+  ]
 };
