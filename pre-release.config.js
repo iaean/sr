e5c05716e9814cf14c4d09ce011a0e16c83eb5e9
iaean <iaean@users.noreply.github.com>
2022-08-15 18:46:49 +0200

    fix(ci): update config

--- pre-release.config.js
+++ pre-release.config.js
@@ -1,5 +1,9 @@
-// npx semantic-release -e pre-release.config.js
+// npx semantic-release -e ./pre-release.config.js
+
+const baseReleaseConfig = require('./release.config.js');
+
 module.exports = {
+  ...baseReleaseConfig,
   skipTag: true,
   plugins: [
     "@semantic-release/commit-analyzer",
---
+++ release.config.js
@@ -0,0 +1,28 @@
+module.exports = {
+  ci: false,
+  branches: [
+    "['+([0-9])?(.{+([0-9]),x}).x",
+    "master",
+    "next",
+    "next-major",
+    { "name": "beta",
+      "prerelease": true },
+    { "name": "alpha",
+      "prerelease": true }
+  ],
+  plugins: [
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    "@semantic-release/changelog",
+    [ "@semantic-release/npm", { npmPublish: false } ],
+    [ "@semantic-release/git", {
+        assets: [
+          "VERSION",
+          "Dockerfile",
+          "CHANGELOG.md",
+          "package.json" ],
+        message: "Bumped new version ${nextRelease.version} [skip ci]" }
+    ],
+    "@semantic-release/github"
+  ]
+}
