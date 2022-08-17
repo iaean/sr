d786e0b88edcedff4e7451d01ae7ba0ca40fcdbd
iaean <iaean@users.noreply.github.com>
2022-08-17 12:10:00 +0200

    fix(ci): change ghr

--- release.js
+++ release.js
@@ -15,10 +15,16 @@ module.exports = {
           "package.json" ],
         message: "Bumped new version ${nextRelease.version} [skip ci]" }
     ],
+    // BuildKit is not able to push back multi arch builds via buildx build --load
+    // We cannot use the plugin for pushing multi arch
     /* [ "@semantic-release-plus/docker", {
         skipLogin: true,
         name: "ghcr.io/iaean/sr:semantic-release" }
     ], */
-    "@semantic-release/github"
+    [ "@semantic-release/github", {
+        assets: [
+          { path: "Dockerfile", label: "DNS3L OpenAPI (Swagger) Specification" } ],
+        addReleases: "top" }
+    ]
   ]
 };
